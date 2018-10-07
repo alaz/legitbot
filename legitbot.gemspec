42cbc63b95a78b8e87fc4e7898e179872eba28c4
Alexander Azarov <azarov@osinka.ru>
2018-10-07 12:00:00 +0300

    🐎 initialize the list of Facebook valid IPs lazily [closes #7]
    Also a method to re-load the IPs is provided, see also #5

--- legitbot.gemspec
+++ legitbot.gemspec
@@ -15,7 +15,7 @@ Gem::Specification.new do |spec|
     "made by a real search engine, not a fake"
 
   spec.required_ruby_version = '>= 2.0.0'
-  spec.add_dependency "irrc"
+  spec.add_dependency "irrc", ">= 0.2.1"
   spec.add_dependency "segment_tree"
   spec.add_development_dependency "rake"
   spec.add_development_dependency "minitest"
--- lib/legitbot/facebook.rb
+++ lib/legitbot/facebook.rb
@@ -1,18 +1,31 @@
-require 'segment_tree'
-require 'irrc'
-require 'monitor'
 require 'ipaddr'
+require 'irrc'
+require 'segment_tree'
 
 module Legitbot
   # https://developers.facebook.com/docs/sharing/webmasters/crawler
 
   class Facebook < BotMatch
-    lock = Monitor.new
-
     AS = 'AS32934'
-    ValidIPs = lock.synchronize do
+
+    def valid?
+      ip = IPAddr.new(@ip)
+      Facebook.valid_ips[ip.ipv4? ? :ipv4 : :ipv6].find(ip)
+    end
+
+    @mutex = Mutex.new
+
+    def self.valid_ips
+      @mutex.synchronize { @ips ||= load_ips }
+    end
+
+    def self.reload!
+      @mutex.synchronize { @ips = load_ips }
+    end
+
+    def self.load_ips
       client = Irrc::Client.new
-      client.query :radb, 'AS32934'
+      client.query :radb, AS
       results = client.perform
 
       Hash[%i(ipv4 ipv6).map { |k|
@@ -20,11 +33,6 @@ module Legitbot
             [IPAddr.new(cidr).to_range, true]
           })]
       }]
-    end
-
-    def valid?
-      ip = IPAddr.new(@ip)
-      ValidIPs[ip.ipv4? ? :ipv4 : :ipv6].find(ip)
     end
   end
 
