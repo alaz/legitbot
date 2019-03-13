413c3d941af090a4d09537ce79b287b13d49a887
Georgi Kirichkov <kirichkov@users.noreply.github.com>
2019-03-13 20:32:45 +0200

    Updates DuckDuckBot IP addresses
    https://duckduckgo.com/duckduckbot

--- lib/legitbot/duckduckgo.rb
+++ lib/legitbot/duckduckgo.rb
@@ -1,7 +1,7 @@
 module Legitbot
   # https://duckduckgo.com/duckduckbot
   class DuckDuckGo < BotMatch
-    ValidIPs = %w(72.94.249.34 72.94.249.35 72.94.249.36 72.94.249.37 72.94.249.38)
+    ValidIPs = %w(107.20.237.51 23.21.226.191 107.21.1.8 54.208.102.37)
 
     def valid?
       DuckDuckGo::ValidIPs.include? @ip
