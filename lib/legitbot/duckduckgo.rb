fb970a4cdd88fa8a445b7e7c82a2ea56f45dfbca
Alexander Azarov <self@alaz.me>
2019-08-21 18:37:18 +0300

    ➡️ update the list of DuckDuckGo IPs
    https://help.duckduckgo.com/duckduckgo-help-pages/results/duckduckbot/

--- lib/legitbot/duckduckgo.rb
+++ lib/legitbot/duckduckgo.rb
@@ -1,7 +1,7 @@
 module Legitbot
   # https://duckduckgo.com/duckduckbot
   class DuckDuckGo < BotMatch
-    ValidIPs = %w(107.20.237.51 23.21.226.191 107.21.1.8 54.208.102.37)
+    ValidIPs = %w(50.16.241.113 50.16.241.114 50.16.241.117 50.16.247.234 52.204.97.54 52.5.190.19 54.197.234.188 54.208.100.253 23.21.227.69)
 
     def valid?
       DuckDuckGo::ValidIPs.include? @ip
