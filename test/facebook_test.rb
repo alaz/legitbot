14cf13d22d8c808a6dd8ca74d8178e563af059b2
Alexander Azarov <self@alaz.me>
2019-10-21 21:34:57 +0300

    🚨 fix the test after #d294e91
    Both Facebook and Twitter are fake bots for the `User-agent`

--- test/facebook_test.rb
+++ test/facebook_test.rb
@@ -55,7 +55,7 @@ class FacebookTest < Minitest::Test
       'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_1) AppleWebKit/601.2.4 (KHTML, like Gecko) Version/9.0.1 Safari/601.2.4 facebookexternalhit/1.1 Facebot Twitterbot/1.0',
       '92.243.181.7'
     ) do |bot|
-      assert_equal :facebook, bot.detected_as
+      assert %i[facebook twitter].include?(bot.detected_as)
       assert bot.fake?, msg: 'fake Facebook'
     end
   end
