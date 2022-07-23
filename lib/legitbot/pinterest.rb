534db5ff84ce369d27dc2cd75412df70ad23bcfc
Alexander Azarov <self@alaz.me>
2022-07-23 09:18:15 +0100

    ➡️ update domains for Pinterest

--- lib/legitbot/pinterest.rb
+++ lib/legitbot/pinterest.rb
@@ -3,7 +3,7 @@
 module Legitbot # :nodoc:
   # https://help.pinterest.com/en/articles/about-pinterest-crawler-0
   class Pinterest < BotMatch
-    domains 'pinterest.com.'
+    domains 'pinterest.com.', 'pinterestcrawler.com.'
   end
 
   rule Legitbot::Pinterest, %w[Pinterestbot Pinterest/0.2]
