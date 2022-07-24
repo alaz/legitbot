1d35a658376d586dde7c6f4d49bca7a6fa565e74
Alexander Azarov <self@alaz.me>
2022-07-24 13:44:28 +0100

    👷 collect and upload code coverage

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,5 +1,11 @@
 # frozen_string_literal: true
 
+require 'simplecov'
+SimpleCov.start
+
+require 'simplecov-cobertura'
+SimpleCov.formatter = SimpleCov::Formatter::CoberturaFormatter
+
 require 'legitbot'
 
 require 'minitest/autorun'
