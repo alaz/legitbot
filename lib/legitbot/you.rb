9b63c552f3a4b6f0df80cc9ca7e4846399b84ee4
Alexander Azarov <self@alaz.me>
2023-08-26 13:44:30 +0100

    Fetch IPs for you.com

--- lib/legitbot/you.rb
+++ lib/legitbot/you.rb
@@ -3,6 +3,8 @@
 module Legitbot # :nodoc:
   # https://about.you.com/youbot/
   class You < BotMatch
+    # @fetch:url https://about.you.com/youbot/
+    # @fetch:selector section.elementor-section-content-top div.elementor-widget-text-editor pre > code
     ip_ranges %w[
       20.59.40.22
     ]
