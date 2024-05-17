ead613298d3dbff6dcd8bd6353ed6d6cc0b67e37
Alexander Azarov <self@alaz.me>
2024-05-17 22:45:49 +0100

    🛠️ disable fetching IPs for GPTBot

--- lib/legitbot/gptbot.rb
+++ lib/legitbot/gptbot.rb
@@ -3,7 +3,8 @@
 module Legitbot # :nodoc:
   # https://platform.openai.com/docs/gptbot
   class GPTBot < BotMatch
-    # @fetch:url https://openai.com/gptbot-ranges.txt
+    # NOTE: fetching has been disabled, see #131
+    # @ fetch:url https://openai.com/gptbot-ranges.txt
     ip_ranges %w[
       52.230.152.0/24
       52.233.106.0/24
